539671ff339486c5bc3c0a681f5103dcdc643872
AbigaailAcuna <acuaabigail@yahoo.com>
2024-04-23 13:02:01 -0600

    Generando pdf de compra

--- controllers/Venta.php
+++ controllers/Venta.php
@@ -1,52 +1,194 @@
 <?php
 session_start();
+require_once './vendor/autoload.php';
+use Dompdf\Dompdf;
+
 //interactuamos entre modelo y vista
 class VentaController
 {
-      public function FinalizarCompra()
-      {
-          $total = 0;
-      
-          require_once "./models/ComprasModelo.php";
+    public function FinalizarCompra()
+    {
+        $total = 0;
+
+        require_once "./models/ComprasModelo.php";
+
+        $productos = new ComprasModelo();
+
+        // Si hay una sesión iniciada y hay productos en el carrito
+        if (isset($_SESSION['login_data']) && isset($_SESSION['carrito'])) {
+            foreach ($_SESSION['carrito'] as $producto) {
+                // Calcula el total
+              //  $total += $producto['CantidadProducto'] * $producto['PrecioUnitario'];
+
+                // Prepara los datos de la compra
+                $compra = [
+                    'IdUsuario' => $_SESSION['login_data']['IdUsuario'],
+                    'IdProducto' => $producto['Id'],
+                    'FechaCompra' => date("Y-m-d"),
+                    'Cantidad' => $producto['Cantidad']
+                ];
+
+                // Guarda la compra en la base de datos
+                $productos->InsertarCompra($compra);
+
+                // Calcula la nueva cantidad disponible del producto
+                $nuevacantidad = $producto['CantidadProducto'] - $producto['Cantidad'];
+
+                // Actualiza la cantidad disponible del producto en la base de datos
+                $productos->ActualizarCantidad($producto['Id'], $nuevacantidad);
+            }
+
+           
+            // Generando PDF
+        try {
+            $html = '<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Confirmación de compra</title>
+    <style>
+        body {
+            font-family: "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
+            background-color: #fff;
+            padding: 30px;
+        }
+        h1 {
+            font-size: 36pt;
+            font-weight: bold;
+            color: #333;
+            text-align: center;
+            margin-bottom: 30px;
+        }
+        h3 {
+            font-size: 24pt;
+            font-weight: bold;
+            color: #333;
+            margin-bottom: 20px;
+        }
+        .coupon {
+            display: inline-block;
+            width: 80%;
+            border: 2px solid #333;
+            background-color: #f2f2f2;
+            padding: 20px;
+            margin-bottom: 30px;
+        }
+        .coupon h3 {
+            font-size: 28pt;
+            margin-bottom: 20px;
+        }
+        .coupon p {
+            font-size: 18pt;
+            margin-bottom: 10px;
+        }
+        .coupon .total {
+            font-size: 24pt;
+            font-weight: bold;
+            margin-top: 20px;
+            text-align: right;
+        }
+        #header {
+            margin-bottom: 30px;
+            text-align: center;
+            border-bottom: 2px solid #333;
+            padding-bottom: 10px;
+        }
+        #header h2 {
+            font-size: 36pt;
+            font-weight: bold;
+            color: #333;
+        }
+        #message {
+            font-size: 18pt;
+            margin-bottom: 20px;
+            text-align: center;
+        }
+        table {
+            border-collapse: collapse;
+            margin-bottom: 40px;
+            width: 100%;
+            padding: 20px;
+            table-layout: fixed;
+        }
+        th, td {
+            border: 1px solid #fff;
+            padding: 12px;
+        }
+        th {
+            background-color: #eee;
+            font-weight: bold;
+        }
+        td {
+            font-weight: 600;
+        }
+        tbody tr:nth-child(even) {
+            background-color: rgba(255, 255, 255, 0.3);
+        }
+        tfoot td {
+            border-top: 2px solid #fff;
+            font-weight: bold;
+            font-size: 16pt;
+            padding-top: 20px;
+        }
+        tfoot td:last-child {
+            font-size: 18pt;
+        }
+    </style>
+</head>
+<body>
+    <div id="header">
+        <h2> Fundación Dame Tu Pata</h2>
+    </div>
+    <div id="message">
+        <p>Gracias por comprar con nosotros, a continuación tienes el detalle de tus productos adquiridos:</p>
+    </div>
+    <h3>Detalles de compra</h3>';
+
+            foreach ($_SESSION['carrito'] as $product) {
+                // Generando un div para separar cada cupon en cuadros que representan cupones
+                $html .= '<div class="coupon">
+      <h3>' . $product['NombreProducto'] . '</h3>
+      <p>Precio: $' . $product['PrecioUnitario'] . '</p>
+      <p>Cantidad: ' . $product['Cantidad'] . '</p>
+  </div>';
+            }
+
+       //    $html .= '<h3>Precio: $' . $total . '</h3>';
+            $html .= '<div style="page-break-after: always;"></div>';
+
+            $dompdf = new Dompdf();
+            // Cargar el contenido dentro de Dompdf
+            $dompdf->loadHtml($html);
+            // Setear el tamaño de página
+            $dompdf->setPaper('A4', 'vertical');
+
+            // Renderizar el PDF
+            $dompdf->render();
+
+            // Generar el nombre de archivo con la fecha y hora actual
+            $filename = 'Producto_' . $_SESSION['login_data']['CorreoUsuario'] . '_' . date('Ymd_His') . '.pdf';
+            // Ruta donde se guardará el archivo PDF
+            $filepath = './Pdf/' . $filename;
+            // Guardar el archivo PDF en la ruta especificada
+            file_put_contents($filepath, $dompdf->output());
+
+            header('Location:?c=Compras&a=index');
+
+            unset($_SESSION['carrito']);
+
+           
+
+            // Redireccionar o mostrar algún mensaje de éxito
+        } catch (\Exception $e) {
+            echo 'Error al generar el PDF: ' . $e->getMessage();
+        }
        
-          $productos = new ComprasModelo();
-      
-        
 
-          // Si hay una sesión iniciada y hay productos en el carrito
-          if (isset($_SESSION['login_data']) && isset($_SESSION['carrito'])) {
-              foreach ($_SESSION['carrito'] as $producto) {
-                  // Calcula el total
-                  $total += $producto['CantidadProducto'] * $producto['PrecioUnitario'];
-      
-                  // Prepara los datos de la compra
-                  $compra = [
-                      'IdUsuario' => $_SESSION['login_data']['IdUsuario'],
-                      'IdProducto' => $producto['Id'],
-                      'FechaCompra' => date("Y-m-d"),
-                      'Cantidad' => $producto['Cantidad']
-                  ];
-      
-                  // Guarda la compra en la base de datos
-                  $productos->InsertarCompra($compra);
-      
-                  // Calcula la nueva cantidad disponible del producto
-                  $nuevacantidad = $producto['CantidadProducto'] - $producto['Cantidad'];
-      
-                  // Actualiza la cantidad disponible del producto en la base de datos
-                  $productos->ActualizarCantidad($producto['Id'], $nuevacantidad);
-              }
-      
-              header('Location:?c=Compras&a=index');
+        require_once "views/cliente/Carrito.php";
 
-              unset($_SESSION['carrito']);
-              exit;
-          } else {
-              var_dump("Error: no hay sesión iniciada o no hay productos en el carrito");
-          }
-
-          require_once "views/cliente/Carrito.php";
-      
-      }
-      
+        } else {
+            var_dump("Error: no hay sesión iniciada o no hay productos en el carrito");
+        }
+       
+           }
 }
+?>
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -4,32 +4,70 @@
 
 namespace Composer\Autoload;
 
-class ComposerStaticInit2185d2f99bcd56787481d9357a5972d3
+class ComposerStaticInit5e75bb1ee9cf83a9611c558a87ae8924
 {
     public static $prefixLengthsPsr4 = array (
+        'S' => 
+        array (
+            'Svg\\' => 4,
+            'Sabberworm\\CSS\\' => 15,
+        ),
         'P' => 
         array (
             'PHPMailer\\PHPMailer\\' => 20,
         ),
+        'M' => 
+        array (
+            'Masterminds\\' => 12,
+        ),
+        'F' => 
+        array (
+            'FontLib\\' => 8,
+        ),
+        'D' => 
+        array (
+            'Dompdf\\' => 7,
+        ),
     );
 
     public static $prefixDirsPsr4 = array (
+        'Svg\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/phenx/php-svg-lib/src/Svg',
+        ),
+        'Sabberworm\\CSS\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/sabberworm/php-css-parser/src',
+        ),
         'PHPMailer\\PHPMailer\\' => 
         array (
             0 => __DIR__ . '/..' . '/phpmailer/phpmailer/src',
+        ),
+        'Masterminds\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/masterminds/html5/src',
+        ),
+        'FontLib\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/phenx/php-font-lib/src/FontLib',
+        ),
+        'Dompdf\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/dompdf/dompdf/src',
         ),
     );
 
     public static $classMap = array (
         'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
+        'Dompdf\\Cpdf' => __DIR__ . '/..' . '/dompdf/dompdf/lib/Cpdf.php',
     );
 
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->prefixLengthsPsr4 = ComposerStaticInit2185d2f99bcd56787481d9357a5972d3::$prefixLengthsPsr4;
-            $loader->prefixDirsPsr4 = ComposerStaticInit2185d2f99bcd56787481d9357a5972d3::$prefixDirsPsr4;
-            $loader->classMap = ComposerStaticInit2185d2f99bcd56787481d9357a5972d3::$classMap;
+            $loader->prefixLengthsPsr4 = ComposerStaticInit5e75bb1ee9cf83a9611c558a87ae8924::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit5e75bb1ee9cf83a9611c558a87ae8924::$prefixDirsPsr4;
+            $loader->classMap = ComposerStaticInit5e75bb1ee9cf83a9611c558a87ae8924::$classMap;
 
         }, null, ClassLoader::class);
     }
